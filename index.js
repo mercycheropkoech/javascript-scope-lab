50990a402ea7a27169350f2bf0fe44f3f6b4f3fb
MERCYCHEROP <mercy.cherop@student.moringaschool.com>
2026-04-14 15:24:16 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+// Global scope variables
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+// Function to add a burger (function scope)
+function addBurger() {
+  const newBurger = 'Flatburger';
+  burgers.push(newBurger);
+}
+
+// Block scope example
+if(true) {
+  const anotherNewBurger = 'Maple Bacon Burger';
+  burgers.push(anotherNewBurger);
+}
+
+// Function to change featured drink
+function changeFeaturedDrink() {
+  featuredDrink = 'The JavaShake';
+}
